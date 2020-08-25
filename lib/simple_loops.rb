cfe01bee45781a029aacbff36c84c90a515d6490
Oscar A Castro <oacastro2@gmail.com>
2020-08-25 22:58:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,20 +8,29 @@ end
 
 
 
-def loop_message_n_times(string, integer)
+def loop_message_n_times(string, limit)
   counter = 0 
-  while string[integer] do 
-    puts string[integer] 
-    counter += integer 
+  while count < limit do 
+    puts string 
+    counter += 1  
   end
 end  
 
 def output_array(array)
-  output_array([array])  
-    
+  count = 0 
+  while count < array.length do
+    puts array[count]
+    count += 1 
+  end   
 end
 
 def return_string_array(array)
+  count = 0 
+  while count < array.length do 
+    array[count].to_s 
+    count += 1 
+  end
+end   
   
   
 
