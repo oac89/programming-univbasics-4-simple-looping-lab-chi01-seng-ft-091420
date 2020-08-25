0d0639a1abcd8343e04b4220baf663615326d9d3
Oscar A Castro <oacastro2@gmail.com>
2020-08-25 22:17:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
-# Write your methods here
+def loop_message_five_times(string)
+  puts string 
+  puts string 
+  puts string 
+  puts string
+  puts string
+end  
+
+
+
+def loop_message_n_times(string, integer)
+  counter = 0 
+  while string[counter] do 
+    puts string[counter] 
+    counter += integer 
+  end
+end  
+
+
